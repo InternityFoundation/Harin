dbd8809d3d13cc6c4c0e8baad6b8ad0e09560d52
Harin Mehta <37559327+harinmehta@users.noreply.github.com>
2019-07-27 12:06:38 +0530

    Add files via upload

---
+++ SecurityEmailDemo/target/classes/com/harinmehta/demo/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.harinmehta.demo.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource securityDataSource;
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception{
+	
+		http
+			.authorizeRequests()
+			.antMatchers("/api/register").permitAll()
+			.antMatchers("/api/login").hasRole("EMPLOYEE")
+			.antMatchers("/api/changePassword").hasRole("EMPLOYEE")
+			.anyRequest().authenticated()
+			.and()
+			.httpBasic()
+			.and()
+			.logout().permitAll()
+			.and()
+			.csrf().disable();
+		
+	}	
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+		
+		auth
+			.jdbcAuthentication().dataSource(securityDataSource);		
+	}
+	
+	@Bean
+	public UserDetailsManager userDetailsManager() {
+		
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+		jdbcUserDetailsManager.setDataSource(securityDataSource);
+		return jdbcUserDetailsManager;
+	}
+	
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception{
+		return super.authenticationManagerBean();
+	}
+	
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
